c3b4a6ff7fbfd7699b4344b99749747b32d81e27
vagrant <vagrant@localhost.localdomain>
2020-10-06 12:23:50 +0000

    change js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,9 +11,9 @@
 // about supported directives.
 //
 //= require rails-ujs
+//= require jquery
 //= require activestorage
 //= require turbolinks
-//= require jquery
 //= require bootstrap-sprockets
 //= require iscroll.js
 //= require drawer.js
